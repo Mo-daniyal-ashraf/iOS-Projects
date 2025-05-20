686fd3d38783a673803b02231b2a45160dbe02ad
daniyal <=>
2025-05-20 14:00:14 +0530

    main actor commit

--- 5 Months iOS Plan/MainActor and UI updates/MainActor and UI updates/ViewController.swift
+++ 5 Months iOS Plan/MainActor and UI updates/MainActor and UI updates/ViewController.swift
@@ -10,9 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        Task.detached {
+            print(Thread.isMainThread ? "Amin" : "back")
+
+            await self.fetchAPi_1()
+        }
+        
+//        fetchAPI_2()
+    }
+    
+//    @MainActor
+    func updateUi(with: String) {
+        
+        self.lbl.text = with
+    }
+    
+    func fetchAPI_2() {
+        
+         URLSession.shared.dataTask(with: URL(string: "https://catfact.ninja/fact")!) { data, response, error in
+         
+            if error != nil && data == nil {
+                return
+            }
+            guard let data = data else { return }
+            
+             do {
+                 let json = try JSONSerialization.jsonObject(with: data)
+                 print(Thread.isMainThread ? "Amin" : "back")
+
+                 if let str = json as? [String:Any],
+                    let catfact = str["fact"] as? String {
+                     
+//                     Task {
+//                        self.updateUi(with: catfact)
+//                     }
+//
+                 }
+                 print("🐱 Cat Fact JSON: \(json)")
+             } catch {
+                 print("❌ JSON decoding error: \(error.localizedDescription)")
+             }
+            
+         }.resume()
+    }
+    
+    
+    func fetchAPi_1() async {
+        
+        do {
+            
+            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://catfact.ninja/fact")!)
+            print(Thread.isMainThread)
+
+            let jsonData = try JSONSerialization.jsonObject(with: data)
+            
+            if let str = jsonData as? [String:Any],
+               let catfact = str["fact"] as? String {
+                
+                
+                updateUi(with: catfact)
+            }
+
+            print(jsonData)
+            
+        } catch let error {
+            
+            print(error.localizedDescription)
+        }
     }
 }
 
